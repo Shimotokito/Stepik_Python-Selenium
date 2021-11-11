caeb9d0848b365608772c5a80c0f9e6eeb25ccac
System Administrator <root@MacBook-Air-Max.local>
2021-11-12 00:49:22 +0300

    add my last test

---
+++ lesson24_step8.py
@@ -0,0 +1,28 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/explicit_wait2.html")
+
+    WebDriverWait(browser, 12).until(
+            EC.text_to_be_present_in_element((By.ID, "price"), "100"))
+    browser.find_element(By.ID, "book").click()
+    x = browser.find_element(By.ID, "input_value").text
+    y = calc(x)
+    input = browser.find_element(By.ID, "answer")
+    input.send_keys(y)
+    browser.find_element(By.CSS_SELECTOR, '[type="submit"]').click()
+
+
+finally:
+    time.sleep(20)
+    browser.quit()
+
